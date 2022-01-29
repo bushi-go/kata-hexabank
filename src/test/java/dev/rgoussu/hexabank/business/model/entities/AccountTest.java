dc434d322848917f13cf9766dfa31220fffb124d
rgoussu <rgoussu.develop@gmail.com>
2022-01-29 13:41:49 +0100

    feat: adding test to take into account the initial balance of the account

--- src/test/java/dev/rgoussu/hexabank/business/model/entities/AccountTest.java
+++ src/test/java/dev/rgoussu/hexabank/business/model/entities/AccountTest.java
@@ -2,6 +2,7 @@ package dev.rgoussu.hexabank.business.model.entities;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.UUID;
@@ -11,11 +12,17 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class AccountTest {
 
     @ParameterizedTest
-    @ValueSource(ints={0,10,15,20,30,45,50,100})
-    public void givenAccountAndDepositShouldAddToBalance(int amount){
+    @CsvSource(value={
+            "0,0",
+            "0,10",
+            "0,15",
+            "0,20",
+            "10,30"
+    })
+    public void givenAccountAndDepositShouldAddToBalance(int initialBalance, int amount){
         String accountId = UUID.randomUUID().toString();
-        Account underTest = Account.builder().accountId(accountId).build();
-        Account expected = Account.builder().accountId(accountId).balance(amount).build();
+        Account underTest = Account.builder().accountId(accountId).balance(initialBalance).build();
+        Account expected = Account.builder().accountId(accountId).balance(initialBalance+amount).build();
         Account actual = underTest.deposit(amount);
         assertEquals(expected, actual);
     }
