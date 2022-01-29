912a0099df0857c7fd49acff9c1df4acbe3978ea
rgoussu <rgoussu.develop@gmail.com>
2022-01-29 18:56:53 +0100

    feat: implemented conversion on deposit of foreign currency

--- src/main/java/dev/rgoussu/hexabank/business/model/types/DepositError.java
+++ src/main/java/dev/rgoussu/hexabank/business/model/types/DepositError.java
@@ -1,5 +1,5 @@
 package dev.rgoussu.hexabank.business.model.types;
 
 public enum DepositError {
-    UNKNOWN_ACCOUNT;
+    UNKNOWN_ACCOUNT, COULD_NOT_CONVERT_TO_ACCOUNT_CURRENCY;
 }
--- src/main/java/dev/rgoussu/hexabank/business/services/AccountOperationManager.java
+++ src/main/java/dev/rgoussu/hexabank/business/services/AccountOperationManager.java
@@ -1,9 +1,11 @@
 package dev.rgoussu.hexabank.business.services;
 
 import dev.rgoussu.hexabank.business.exceptions.NoSuchAccountException;
+import dev.rgoussu.hexabank.business.exceptions.UnavailableExchangeRateException;
 import dev.rgoussu.hexabank.business.model.dto.DepositResult;
 import dev.rgoussu.hexabank.business.model.entities.Account;
 import dev.rgoussu.hexabank.business.model.values.Money;
+import dev.rgoussu.hexabank.business.ports.driven.ExchangeRateProviderPort;
 import dev.rgoussu.hexabank.business.ports.driven.AccountPersistencePort;
 
 /**
@@ -12,21 +14,31 @@ import dev.rgoussu.hexabank.business.ports.driven.AccountPersistencePort;
 public class AccountOperationManager implements AccountOperationService{
 
     private final AccountPersistencePort accountPersistencePort;
-
-    public AccountOperationManager(AccountPersistencePort accountPersistencePort) {
+    private final ExchangeRateProviderPort exchangeRateProviderPort;
+    public AccountOperationManager(AccountPersistencePort accountPersistencePort, ExchangeRateProviderPort exchangeRateProviderPort) {
         this.accountPersistencePort = accountPersistencePort;
+        this.exchangeRateProviderPort = exchangeRateProviderPort;
     }
 
     @Override
     public DepositResult processDeposit(String accountId, Money deposit) {
         try {
             Account account = accountPersistencePort
-                    .findByAccountId(accountId)
-                    .deposit(deposit);
+                    .findByAccountId(accountId);
+            Money finalDeposit;
+            if(account.getOperatingCurrency().equals(deposit.getCurrency())){
+               finalDeposit = deposit;
+            }else{
+                double exchangeRate = exchangeRateProviderPort.getExchangeRateForCurrencies(deposit.getCurrency(), account.getOperatingCurrency());
+                finalDeposit = deposit.convert(account.getOperatingCurrency(), exchangeRate);
+            }
+            account = account.deposit(finalDeposit);
             accountPersistencePort.save(account);
             return DepositResult.success(accountId, account.getBalance());
         }catch(NoSuchAccountException e){
             return DepositResult.noSuchAccount(accountId);
+        }catch(UnavailableExchangeRateException e){
+            return DepositResult.unavailableExchangeRate(accountId);
         }
     }
 }
