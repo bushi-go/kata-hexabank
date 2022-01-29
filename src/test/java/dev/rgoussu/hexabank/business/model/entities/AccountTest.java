30111a03f62e57a33d650e672c81182afcfb97b7
rgoussu <rgoussu.develop@gmail.com>
2022-01-29 15:44:29 +0100

    feat: refactored to be able to specify the currency of operation of the account at account creation

--- src/test/java/dev/rgoussu/hexabank/business/model/entities/AccountTest.java
+++ src/test/java/dev/rgoussu/hexabank/business/model/entities/AccountTest.java
@@ -23,8 +23,8 @@ public class AccountTest {
     @MethodSource("generateDeposits")
     public void givenAccountAndDepositShouldAddToBalance(Money initialBalance, Money amount){
         String accountId = UUID.randomUUID().toString();
-        Account underTest = Account.builder().accountId(accountId).balance(initialBalance).build();
-        Account expected = Account.builder().accountId(accountId).balance(initialBalance.plus(amount)).build();
+        Account underTest = Account.create(accountId,initialBalance);
+        Account expected = Account.create(accountId,initialBalance.plus(amount));
         Account actual = underTest.deposit(amount);
         assertEquals(expected, actual);
     }
@@ -32,8 +32,18 @@ public class AccountTest {
     @Test
     public void givenNegativeMoneyShouldThrow(){
         String accountId = UUID.randomUUID().toString();
-        Account underTest = Account.builder().accountId(accountId).balance(Money.get(0, Currency.EUR)).build();
+        Account underTest = Account.create(accountId,Money.get(0, Currency.EUR));
         assertThrows(IllegalArgumentException.class, ()->underTest.deposit(Money.get(-10, Currency.EUR)));
+    }
+
+    @Test
+    public void givenUninitializedBalanceShouldInitializeAndProcessDeposit(){
+        String accountId = UUID.randomUUID().toString();
+        Account underTest = Account.create(accountId);
+        Money deposit = Money.get(10, Currency.EUR);
+        Account expected = Account.create(accountId,deposit);
+        Account actual = underTest.deposit(deposit);
+        assertEquals(expected, actual);
     }
 
     public static Stream<Arguments> generateDeposits() {
