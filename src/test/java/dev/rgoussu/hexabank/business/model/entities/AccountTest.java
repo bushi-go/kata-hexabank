56ce82e7fb01b18a903039084b2cbe966bf0a734
rgoussu <rgoussu.develop@gmail.com>
2022-01-29 14:54:10 +0100

    feat: refactored test to make use of money class in account

--- src/test/java/dev/rgoussu/hexabank/business/model/entities/AccountTest.java
+++ src/test/java/dev/rgoussu/hexabank/business/model/entities/AccountTest.java
@@ -1,30 +1,42 @@
 package dev.rgoussu.hexabank.business.model.entities;
 
+import dev.rgoussu.hexabank.business.model.types.Currency;
+import dev.rgoussu.hexabank.business.model.values.Money;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.UUID;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class AccountTest {
 
+
+
     @ParameterizedTest
-    @CsvSource(value={
-            "0,0",
-            "0,10",
-            "0,15",
-            "0,20",
-            "10,30"
-    })
-    public void givenAccountAndDepositShouldAddToBalance(int initialBalance, int amount){
+    @MethodSource("generateDeposits")
+    public void givenAccountAndDepositShouldAddToBalance(Money initialBalance, Money amount){
         String accountId = UUID.randomUUID().toString();
         Account underTest = Account.builder().accountId(accountId).balance(initialBalance).build();
-        Account expected = Account.builder().accountId(accountId).balance(initialBalance+amount).build();
+        Account expected = Account.builder().accountId(accountId).balance(initialBalance.plus(amount)).build();
         Account actual = underTest.deposit(amount);
         assertEquals(expected, actual);
     }
 
+    public static Stream<Arguments> generateDeposits() {
+        return Stream.of(
+                Arguments.of(Money.get(0, Currency.EUR), Money.get(10, Currency.EUR)),
+
+                Arguments.of(Money.get(0, Currency.EUR), Money.get(20, Currency.EUR)),
+
+                Arguments.of(Money.get(0, Currency.EUR), Money.get(30, Currency.EUR)),
+
+                Arguments.of(Money.get(10, Currency.EUR), Money.get(100, Currency.EUR))
+        );
+    }
 }
