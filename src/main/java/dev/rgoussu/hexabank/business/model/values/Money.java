0dd3c81350708886869c8f7df9bfbf76ebd48c35
rgoussu <rgoussu.develop@gmail.com>
2022-01-29 14:41:34 +0100

    feat: added money and currency class, and an addition method

---
+++ src/main/java/dev/rgoussu/hexabank/business/model/values/Money.java
@@ -0,0 +1,28 @@
+package dev.rgoussu.hexabank.business.model.values;
+
+import dev.rgoussu.hexabank.business.model.types.Currency;
+import lombok.*;
+
+import java.math.BigDecimal;
+
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@EqualsAndHashCode
+public class Money {
+    public BigDecimal amount;
+    public Currency currency;
+
+    public static Money get(Number amount, Currency currency){
+        return new Money(BigDecimal.valueOf(amount.doubleValue()), currency);
+    }
+
+    public Money plus(Money other){
+        return Money.get(other.amount.add(this.amount), currency);
+    }
+
+    @Override
+    public String toString() {
+        return amount.toString() +" "+currency.getSymbol();
+    }
+
+}
